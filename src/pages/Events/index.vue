42787360ba707acdbdeef6838c016ecc654e76ad
rezuki <rezky.wahyuda.tif18@polban.ac.id>
2021-03-18 23:56:14 +0800

    add form create event but without date

--- src/pages/Events/index.vue
+++ src/pages/Events/index.vue
@@ -7,8 +7,154 @@
       </div>
 
       <div class="flex justify-end">
-        <a href="#" class="bg-indigo-600 hover:bg-indigo-400 hover:text-white border border-gray-200 text-gray-200 font-bold py-2 px-4 rounded-lg">Tambah Event</a>
+        <button
+        @click="open = true"
+        class="bg-indigo-600 hover:bg-indigo-400 hover:text-white border border-gray-200 text-gray-200 font-bold py-2 px-4 rounded-lg"
+        >
+        Tambah Event
+        </button>
       </div>
+
+      <div
+      :class="`modal ${
+        !open && 'opacity-0 pointer-events-none'
+      } z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center`"
+    >
+      <div
+        class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"
+      ></div>
+
+      <div
+        class="modal-container fixed bg-white w-1/8 2xl:w-1/3 md:w-1/2 sm:w-1/7 mx-auto h-1/7 xl:h-1/2 md:h-1/3 sm:h-1/4 rounded shadow-lg z-50 overflow-y-auto"
+      >
+        <div
+          class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"
+        >
+          <svg
+            class="fill-current text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+          >
+            <path
+              d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+            />
+          </svg>
+          <span class="text-sm">(Esc)</span>
+        </div>
+
+        <!-- Add margin if you want to see some of the overlay behind the modal-->
+        <div class="modal-content py-4 text-left px-6">
+          <!--Title-->
+          <div class="flex justify-between items-center pb-3">
+            <p class="text-2xl font-bold">Buat Event</p>
+            <div class="modal-close cursor-pointer z-50" @click="open = false">
+              <svg
+                class="fill-current text-black"
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                />
+              </svg>
+            </div>
+          </div>
+
+          <!--Body-->
+          <form class="w-full max-w-lg" @submit.prevent>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/4">
+                <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                  Nama Kegiatan
+                </label>
+              </div>
+              <div class="md:w-3/4">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" id="inline-full-name" type="text" placeholder="Masukkan nama event">
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/4">
+                <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                  Kategori
+                </label>
+              </div>
+              <div class="inline-block relative w-64">
+                <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                  <option disabled selected>Pilih kategori</option>
+                    <option v-for="(kategori, index) in dataKategori" :key="index">
+                      {{ kategori.nama }}
+                    </option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/4">
+                <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                  Tanggal
+                </label>
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/4">
+                <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                  Max Peserta
+                </label>
+              </div>
+              <div class="flex flex-row h-10 w-40 rounded-lg relative bg-transparent mt-1">
+                <button @click="decreaseNumber" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                  <span class="m-auto text-2xl font-thin">−</span>
+                </button>
+                <input type="text" class="outline-none focus:outline-none text-center w-full bg-gray-100 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700" name="custom-input-number" v-bind:value="maxPeserta">
+                <button @click="increaseNumber" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                  <span class="m-auto text-2xl font-thin">+</span>
+                </button>
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/4">
+                <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                  Pemateri
+                </label>
+              </div>
+              <div class="inline-block relative w-64">
+                <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                  <option disabled selected>Pilih pemateri</option>
+                    <option v-for="(pemateri, index) in dataPemateri" :key="index">
+                      {{ pemateri.nama }}
+                    </option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+          </form>
+
+          <!--Footer-->
+          <div class="flex justify-end pt-2">
+            <button
+              @click="open = false"
+              class="px-6 py-3 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+            >
+              Close
+            </button>
+            <button
+              @click="open = false"
+              class="px-6 py-3 bg-indigo-600 rounded-md text-white font-medium tracking-wide hover:bg-indigo-500"
+            >
+              Action
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 
     <div class="mt-8"></div>
@@ -156,9 +302,78 @@ export default defineComponent({
       getDataAcara();
     })
 
+    const kategori = reactive({
+        "id": null,
+        "nama": "",
+        "tanggal": "",
+        "maksPeserta": "",
+        "pemateri": [
+            {
+                "id": null,
+                "nama": "",
+                "foto": ""
+            }
+        ],
+        "kategori": ""
+    });
+
+    const dataKategori = ref([kategori]);
+
+    const getDataKategori = async()=>{
+      let { data } = await axios.get("/kategori");
+      dataKategori.value = data;
+    }
+
+    onMounted(()=>{
+      getDataKategori();
+    })
+
+    const pemateri = reactive({
+            "id": null,
+            "nama": "",
+            "foto": "",
+            "deskripsi": ""
+        });
+
+    const dataPemateri = ref([pemateri]);
+
+    const getDataPemateri = async()=>{
+      let { data } = await axios.get("/pemateri");
+      dataPemateri.value = data;
+    }
+
+    onMounted(()=>{
+      getDataPemateri();
+    })
+
+    const maxPeserta = ref(0);
+
+    const increaseNumber = () => {
+      maxPeserta.value++;
+    }
+
+    const decreaseNumber = () => {
+      if(maxPeserta.value > 0)
+        maxPeserta.value--;
+    }
+
+    const open = ref(false);
+
     return{
-      dataAcara
+      dataAcara,
+      dataKategori,
+      dataPemateri,
+      maxPeserta,
+      increaseNumber,
+      decreaseNumber,
+      open
     }
   },
 });
 </script>
+
+<style>
+.modal {
+  transition: opacity 0.25s ease;
+}
+</style>
