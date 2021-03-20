bf00d53fd5ffc023b12f30f398e665e886f13d13
rezuki <rezky.wahyuda.tif18@polban.ac.id>
2021-03-20 15:03:44 +0800

    setup linting and prettier

---
+++ eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "parser": "babel-eslint"
+  },
+  "rules": {},
+  "extends": [
+  "plugin:vue/vue3-essential",
+  "@vue/prettier", // ADD
+  "eslint:recommended"
+  ]
+}
