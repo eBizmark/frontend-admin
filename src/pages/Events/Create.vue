3e30eab1ab18aa7758acd01abb53a17c8d6ac829
rezuki <rezky.wahyuda.tif18@polban.ac.id>
2021-04-25 22:25:33 +0800

    adjusting api data for create an event

--- src/pages/Events/Create.vue
+++ src/pages/Events/Create.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <form
-      class="w-full m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      @submit.prevent="createKegiatan"
+        class="w-full m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        @submit.prevent="createKegiatan"
     >
       <div class="flex justify-start">
         <h4 class="text-gray-700 text-3xl font-medium mb-6">
@@ -12,54 +12,54 @@
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/6">
           <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="namaKegiatan"
+              class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+              for="namaKegiatan"
           >
             Nama Kegiatan
           </label>
         </div>
         <input
-          class="inline-block relative w-64 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="namaKegiatan"
-          type="text"
-          placeholder="Masukkan nama event"
-          v-model="formData.nama"
+            class="inline-block relative w-64 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="namaKegiatan"
+            type="text"
+            placeholder="Masukkan nama event"
+            v-model="formData.nama"
         />
       </div>
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/6">
           <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="kategori"
+              class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+              for="kategori"
           >
             Kategori
           </label>
         </div>
         <div id="v-model-select-dynamic" class="inline-block relative w-64">
           <select
-            class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-            id="kategori"
-            v-model="formData.kategori"
+              class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              id="kategori"
+              v-model="formData.kategori"
           >
             <option disabled value="">Pilih kategori</option>
             <option
-              v-for="(kategori, index) in dataKategori"
-              :key="index"
-              :value="kategori.nama"
+                v-for="(kategori, index) in dataKategori"
+                :key="index"
+                :value="kategori.nama"
             >
               {{ kategori.nama }}
             </option>
           </select>
           <div
-            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
           >
             <svg
-              class="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
             >
               <path
-                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
               />
             </svg>
           </div>
@@ -68,50 +68,50 @@
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/6">
           <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="tanggal"
+              class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+              for="tanggal"
           >
             Tanggal
           </label>
         </div>
         <div class="inline-block relative w-64">
           <litepie-datepicker
-            as-single
-            :formatter="formatter"
-            id="tanggal"
-            v-model="formData.tanggal"
+              as-single
+              :formatter="formatter"
+              id="tanggal"
+              v-model="formData.tanggal"
           ></litepie-datepicker>
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/6">
           <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="maxPeserta"
+              class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+              for="maxPeserta"
           >
             Max Peserta
           </label>
         </div>
         <div
-          class="flex flex-row h-10 w-40 rounded-lg relative bg-transparent mt-1"
+            class="flex flex-row h-10 w-40 rounded-lg relative bg-transparent mt-1"
         >
           <button
-            type="button"
-            @click="decreaseNumber"
-            class="bg-gray-300 text-black hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+              type="button"
+              @click="decreaseNumber"
+              class="bg-gray-300 text-black hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
           >
             <span class="m-auto text-2xl font-bold">−</span>
           </button>
           <input
-            type="text"
-            class="outline-none focus:outline-none text-center w-full bg-white-200 appearance-none border-2 border-gray-200 rounded font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700 mx-2 focus:border-indigo-500"
-            id="maxPeserta"
-            v-model="formData.maksPeserta"
+              type="text"
+              class="outline-none focus:outline-none text-center w-full bg-white-200 appearance-none border-2 border-gray-200 rounded font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700 mx-2 focus:border-indigo-500"
+              id="maxPeserta"
+              v-model="formData.maksPeserta"
           />
           <button
-            type="button"
-            @click="increaseNumber"
-            class="bg-gray-300 text-black hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+              type="button"
+              @click="increaseNumber"
+              class="bg-gray-300 text-black hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
           >
             <span class="m-auto text-2xl font-bold">+</span>
           </button>
@@ -120,8 +120,8 @@
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/6">
           <label
-            class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-            for="inline-full-name"
+              class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
           >
             Pemateri
           </label>
@@ -129,12 +129,12 @@
         <div class="inline-block relative w-64">
           <div>
             <Multiselect
-              v-model="formData.pemateri"
-              mode="tags"
-              :searchable="true"
-              :createTag="true"
-              :options="multiselectData"
-              placeholder="Pilih pemateri"
+                v-model="formData.pemateri"
+                mode="tags"
+                :searchable="true"
+                :createTag="true"
+                :options="multiselectData"
+                placeholder="Pilih pemateri"
             />
           </div>
         </div>
@@ -143,14 +143,14 @@
       <!--Footer-->
       <div class="flex justify-end pt-2">
         <router-link
-          to="/events"
-          type="button"
-          class="px-6 py-3 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+            to="/events"
+            type="button"
+            class="px-6 py-3 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
         >
           Kembali
         </router-link>
         <button
-          class="px-6 py-3 bg-indigo-600 rounded-md text-white font-medium tracking-wide hover:bg-indigo-500"
+            class="px-6 py-3 bg-indigo-600 rounded-md text-white font-medium tracking-wide hover:bg-indigo-500"
         >
           Submit
         </button>
@@ -182,7 +182,7 @@ export default defineComponent({
 
     const getDataKategori = async () => {
       let { data } = await axios.get('/kategori')
-      dataKategori.value = data
+      dataKategori.value = data.data
     }
 
     const pemateri = reactive({
@@ -203,7 +203,8 @@ export default defineComponent({
 
     const getDataPemateri = async () => {
       let { data } = await axios.get('/pemateri')
-      dataPemateri.value = data
+      dataPemateri.value = data.data
+      // console.log(dataPemateri.value)
       multiselectData.value = modifyKey(dataPemateri.value)
     }
 
@@ -251,9 +252,9 @@ export default defineComponent({
 
     const createKegiatan = async () => {
       await axios
-        .post('/event', formData)
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error))
+          .post('/event', formData)
+          .then((response) => console.log(response))
+          .catch((error) => console.log(error))
       // await router.push({ name: 'Events' })
       await router.push('/events')
       // window.location.reload()
